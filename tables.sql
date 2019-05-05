04701291ef404f125832e2d3bf42d57f9dc61891
Patenaude, Christopher T <patenauc@oregonstate.edu>
2019-05-05 13:45:37 -0700

    added race table and race foreign key to characters

--- tables.sql
+++ tables.sql
@@ -10,29 +10,41 @@ DROP TABLE IF EXISTS `feature`;
 DROP TABLE IF EXISTS `skill`;
 DROP TABLE IF EXISTS `race`;
 
+CREATE TABLE `race` (
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`name`)
+  );
+
+LOCK TABLES `race` WRITE;
+INSERT INTO `race` VALUES ('Dwarf', 'A shorty, just dont mess with the beard');
+INSERT INTO `race` VALUES ('Human', 'Average Joe Shmoe');
+UNLOCK TABLES;
+
 CREATE TABLE `characters` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `race_id` int(11) NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `gender` varchar(255) NOT NULL,
-    `alignment` varchar(255) DEFAULT '',
-    `armor-class` varchar(255),
-    `initiative` int(11) DEFAULT '0',
-    `speed` int(11) DEFAULT '0',
-    `healthpoints` int(11) DEFAULT '0',
-    `inspiration` int(11) DEFAULT '0',
-    `strength` int(11) DEFAULT '0',
-    `dexterity` int(11) DEFAULT '0',
-    `constitution` int(11) DEFAULT '0',
-    `intelligence` int(11) DEFAULT '0',
-    `wisdom` int(11) DEFAULT '0',
-    `charisma` int(11) DEFAULT '0',
-    PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `race_id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `alignment` varchar(255) DEFAULT '',
+  `armor-class` varchar(255),
+  `initiative` int(11) DEFAULT '0',
+  `speed` int(11) DEFAULT '0',
+  `healthpoints` int(11) DEFAULT '0',
+  `inspiration` int(11) DEFAULT '0',
+  `strength` int(11) DEFAULT '0',
+  `dexterity` int(11) DEFAULT '0',
+  `constitution` int(11) DEFAULT '0',
+  `intelligence` int(11) DEFAULT '0',
+  `wisdom` int(11) DEFAULT '0',
+  `charisma` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `characters-1` FOREIGN KEY (`race_id`) REFERENCES `race` (`name`) ON DELETE CASCADE
 );
 
 LOCK TABLES `characters` WRITE;
-INSERT INTO `characters` VALUES (1, 1, 'High-Prophet Chad, the Rust Developer', 'Alpha Male', 'Chaotic Good', 'plate', 99, 99, 99, 99, 99, 99, 99, 99, 99, 99);
-INSERT INTO `characters` VALUES (2, 1, 'Looser Paul, the Virgin C++ Coder', 'Beta-Cuck', 'No One Cares', 'cloth', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+INSERT INTO `characters` VALUES (NULL, 'Human', 'High-Prophet Chad, the Rust Developer', 'Alpha Male', 'Chaotic Good', 'plate', 99, 99, 99, 99, 99, 99, 99, 99, 99, 99);
+INSERT INTO `characters` VALUES (NULL, 'Dwarf', 'Looser Paul, the Virgin C++ Coder', 'Beta-Cuck', 'No One Cares', 'cloth', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 UNLOCK TABLES;
 
 CREATE TABLE `class` (
@@ -49,11 +61,11 @@ INSERT INTO `class` VALUES ('Peasant', '10', '');
 UNLOCK TABLES;
 
 CREATE TABLE `character_class` (
-    `character_id` int(11) NOT NULL,
-    `class_id` varchar(255) NOT NULL,
-    PRIMARY KEY (`character_id`, `class_id`),
-    CONSTRAINT `character_class-1` FOREIGN KEY (`character_id`) REFERENCES `characters` (`id`) ON DELETE CASCADE,
-    CONSTRAINT `character_class-2` FOREIGN KEY (`class_id`) REFERENCES `class` (`name`) ON DELETE CASCADE
+  `character_id` int(11) NOT NULL,
+  `class_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`character_id`, `class_id`),
+  CONSTRAINT `character_class-1` FOREIGN KEY (`character_id`) REFERENCES `characters` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `character_class-2` FOREIGN KEY (`class_id`) REFERENCES `class` (`name`) ON DELETE CASCADE
 );
 
 LOCK TABLES `character_class` WRITE;
@@ -64,18 +76,16 @@ UNLOCK TABLES;
 
 
 /*
-CREATE TABLE `class_feature` (
-);
+  CREATE TABLE `class_feature` (
+  );
 
-CREATE TABLE `feature` (
-);
+  CREATE TABLE `feature` (
+  );
 
-CREATE TABLE `character_skill` (
-);
+  CREATE TABLE `character_skill` (
+  );
 
-CREATE TABLE `skill` (
-);
+  CREATE TABLE `skill` (
+  );
 
-CREATE TABLE `race` (
-);
-*/
+ */
