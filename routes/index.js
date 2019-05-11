53db18b5713c3bd541eed185b4241df2a83d7aa2
Patenaude, Christopher T <patenauc@oregonstate.edu>
2019-05-10 21:04:55 -0700

    finished refactering views and updated some html

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,26 @@
 const router = require('express').Router();
-const paths = require('./paths');
+const page_config = require('./page_config');
 const testData = require('../testData');
+
+const paths = Object.keys(page_config);
 
 router.get('/', (req, res, next) => {
     res.status(200).render('home',{
-        data: paths
+        paths: paths
     });
 });
 
-router.get('/:path', (req, res, next) => {
-    let path = req.params.path;
-    if(paths[path]){
+router.get('/:page', (req, res, next) => {
+    let page = req.params.page;
+    let params = page_config[page];
+    if(params){
         res.status(200).render('table_page', {
+            paths: paths,
             // Path specific parameters such as page title
-            params: paths[path],
+            params: params,
             // Path specific data: will be replaced by database
             // queries in the future
-            data: testData[`${path}`]
+            data: testData[`${page}`]
         });
     } else {
         next();
