5242c32694047622f9500d10a524e2e7b4fb9f9d
khlam <khlam@users.noreply.github.com>
2019-05-19 12:14:25 -0700

    Partial search working for all tables. Multiple columns also searchable as defined in page_config.json

--- routes/index.js
+++ routes/index.js
@@ -36,11 +36,19 @@ router.get('/search/:db_Name/:query', (req, res, next) => {
     let dbName = req.params.db_Name
     let query = req.params.query
     let criteria = page_config[dbName].search
+    let searchCols = ""
+    for (i in criteria) {
+        searchCols = searchCols + criteria[i] + ` LIKE '%${query}%'`
+        if (i != (criteria.length - 1))
+        {
+            searchCols = searchCols + " OR "
+        }
+    }
 
     if (page_config[dbName]) {
-        let queryStr = `SELECT * FROM ${dbName} WHERE ${criteria} LIKE '%${query}%'`
+        let queryStr = `SELECT * FROM ${dbName} WHERE ${searchCols}`
         let db = req.app.get('db');
-
+        console.log(queryStr)
         db.pool.getConnection()
         .then (conn => {
             conn.query(queryStr)
